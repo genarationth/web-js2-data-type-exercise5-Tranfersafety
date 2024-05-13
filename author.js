5f7a4cb26088fad75c51f2a73d84a54b188c0564
Tranfersafety <ratthaphumi.safe@gmail.com>
2024-05-13 14:38:36 +0700

    first commit

---
+++ author.js
@@ -0,0 +1,19 @@
+const author = {
+  authorId: 1,
+  name: "safe",
+  address: {
+    houseNo: "1/1",
+    street: "baker",
+    country: "England",
+    postcode: 999,
+  },
+  url: "google.com",
+};
+console.log(author);
+console.log(author.authorId);
+console.log(author.name);
+console.log(author.address.houseNo);
+console.log(author.address.street);
+console.log(author.address.country);
+console.log(author.address.postcode);
+console.log(author.url);
